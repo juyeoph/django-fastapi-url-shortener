c34dc5a69b5ca41e9574a04e3c6399888bce4262
juyeoph <hyunjuyup@naver.com>
2025-07-03 23:08:21 +0900

    사용자 인증 흐름 및 핵심 기능 테스트 코드 추가

--- shortener/tests.py
+++ shortener/tests.py
@@ -1,10 +1,10 @@
 import pytest
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from .models import URL
 
 @pytest.mark.django_db
 def test_signup_flow(client):
-
     signup_url = reverse('signup')
     response = client.get(signup_url)
 
@@ -47,7 +47,6 @@ def test_signup_fail_password_mismatch(client):
 
 @pytest.mark.django_db
 def test_signup_fail_password_too_common(client):
-
     signup_url = reverse('signup')
     username = 'testuser3'
 
@@ -68,7 +67,6 @@ def test_signup_fail_password_too_common(client):
 
 @pytest.mark.django_db
 def test_signup_fail_username_exists(client):
-
     existing_user = get_user_model().objects.create_user(
         username='existinguser',
         password='testpassword123!',
@@ -87,3 +85,43 @@ def test_signup_fail_username_exists(client):
     assert b'A user with that username already exists.' in response.content
 
     assert get_user_model().objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_my_urls_view_unauthenticated(client):
+    my_urls_url = reverse('my_urls')
+    response = client.get(my_urls_url)
+
+    assert response.status_code == 302
+    assert '/login/' in response.url
+
+
+@pytest.mark.django_db
+def test_url_creation_and_list_view(client):
+    User = get_user_model()
+    user_a = User.objects.create_user(username='user_a', password='password123!')
+    user_b = User.objects.create_user(username='user_b', password='password123!')
+
+    URL.objects.create(
+        original_url='https://google.com',
+        owner=user_a,
+    )
+    URL.objects.create(
+        original_url='https://naver.com',
+        owner=user_b,
+    )
+    client.login(username='user_a', password='password123!')
+    my_urls_url = reverse('my_urls')
+
+    response = client.get(my_urls_url)
+    assert response.status_code == 200
+
+    assert b'https://google.com' in response.content
+    assert b'https://naver.com' not in response.content
+
+    response = client.post(my_urls_url, {'original_url': 'https://youtube.com'})
+
+    assert response.status_code == 302
+    assert response.url == my_urls_url
+
+    assert URL.objects.filter(owner=user_a, original_url='https://youtube.com').exists()
